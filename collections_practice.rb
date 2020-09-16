ab008b2d82c42c356e8503046d66773c9f50b4c7
Anthony Leon <acidknights89@gmail.com>
2020-09-16 02:09:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |index, word|
+  array.each_with_index.collect do |word, index|
     if index == 1 
       word
     else 
